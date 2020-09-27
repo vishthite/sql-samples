77bddbf950ddad9a1af1cc98f67f00ff890e4baa
Vishal Thite <vishalthite4444@gmail.com>
2020-09-27 17:10:20 +0530

    insert queries

---
+++ insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO country (country_name, country_name_eng, country_code) VALUES ('Deutschland', 'Germany', 'DEU');
+INSERT INTO country (country_name, country_name_eng, country_code) VALUES ('Srbija', 'Serbia', 'SRB');
+INSERT INTO country (country_name, country_name_eng, country_code) VALUES ('Hrvatska', 'Croatia', 'HRV');
+INSERT INTO country (country_name, country_name_eng, country_code) VALUES ('United Stated of America', 'United Stated of America', 'USA');
+INSERT INTO country (country_name, country_name_eng, country_code) VALUES ('Polska', 'Poland', 'POL');
